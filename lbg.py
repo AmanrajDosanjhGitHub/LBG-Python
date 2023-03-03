61eae45e45bc6e954dbded7e5047f2f5f45f1909
andyjjones567 <andy@andyjjones.co.uk>
2023-03-03 10:27:32 +0000

    AJ added to description field

--- lbg.py
+++ lbg.py
@@ -79,7 +79,7 @@ def create():
     # create an item from the Posted request's body data
     posted_data = request.get_json()
     name = posted_data['name']
-    description = posted_data['description']
+    description = posted_data['description-AJ']
     price = posted_data['price']
     item = item_builder(name, description, float(price), int(_id))
 
